1b249a49e056fa2bf71f2e8eba63c81d37618bb5
chernev <kijyra@gmail.com>
2023-07-25 12:06:38 +0300

    edited comments

--- player.py
+++ player.py
@@ -4,15 +4,15 @@ class Player():
 
     def __init__(self, window):
         """инициализация класса Player"""
-        self.window = window # окно игры
+        self.window = window  # окно игры
         self.image = pygame.image.load('images/player.png') # png игрока
-        self.rect = self.image.get_rect() # получаем прямоугольник с координатами из картинки игрока
-        self.window_rect = window.get_rect() # получаем прямоугольник с координатами окна
-        self.rect.centerx = self.window_rect.centerx # задаём координаты по горизонтали
-        self.center = float(self.rect.centerx) # задаём переменную float для более плавного движения
-        self.rect.bottom = self.window_rect.bottom - 20 # задаём координаты по вертикали
-        self.move_right = False # зажата ли кнопка направо?
-        self.move_left = False # зажата ли кнопка налево?
+        self.rect = self.image.get_rect()  # получаем прямоугольник с координатами из картинки игрока
+        self.window_rect = window.get_rect()  # получаем прямоугольник с координатами окна
+        self.rect.centerx = self.window_rect.centerx  # задаём координаты по горизонтали
+        self.center = float(self.rect.centerx)  # задаём переменную float для более плавного движения
+        self.rect.bottom = self.window_rect.bottom - 20  # задаём координаты по вертикали
+        self.move_right = False  # зажата ли кнопка направо?
+        self.move_left = False  # зажата ли кнопка налево?
 
     def update(self):
         """отслеживание перемещения текстуры игрока"""
